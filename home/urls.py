28bcc9ab5a0e8991fcae27b192a874e058d7bc0a
jannatulnayem <jannatulnayem@sslwireless.com>
2025-07-09 01:13:02 +0600

    update

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('menu/', views.menu, name='menu'),
+    path('chef/', views.chef, name='chef'),
+    path('feature/', views.feature, name='feature'),
+    path('blog/', views.blog, name='blog'),
+    path('testimonial/', views.testimonial, name='testimonial'),
+    path('contact/', views.contact, name='contact'),        
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,47 @@ def home(request):
     Render the home page.
     """
     return render(request, 'home/home.html')
+
+
+def about(request):
+    """
+    Render the about page.
+    """
+    return render(request, 'home/about.html')   
+
+def menu(request):
+    """
+    Render the menu page.
+    """
+    return render(request, 'home/menu.html')    
+
+def chef(request):
+    """
+    Render the chef page.
+    """
+    return render(request, 'home/chef.html')    
+
+def feature(request):
+    """
+    Render the feature page.
+    """
+    return render(request, 'home/feature.html') 
+
+def blog(request):
+    """
+    Render the blog page.
+    """
+    return render(request, 'home/blog.html')    
+
+def testimonial(request):
+    """
+    Render the testimonial page.
+    """
+    return render(request, 'home/testimonial.html') 
+
+
+def contact(request):
+    """
+    Render the contact page.
+    """
+    return render(request, 'home/contact.html') 
